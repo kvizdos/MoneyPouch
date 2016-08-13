c3fea1d3c7dd5a7e7819dcc4390b4c570b6a3939
Kenton Vizdos <kvizdos@gmail.com>
2016-08-13 13:40:57 -0400

    Protection

--- src/com/Kento/StrukCraftMoneyPouch/Main.java
+++ src/com/Kento/StrukCraftMoneyPouch/Main.java
@@ -1,28 +1,9 @@
-/* Copywrite Kenton Vizdos
+/* Copyright Kenton Vizdos
  * 2016-~
  * github.com/kvizdos
  */ 
 
 package com.Kento.StrukCraftMoneyPouch;
-
-import java.util.ArrayList;
-
-import org.bukkit.Bukkit;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
 
